d293abf2a74fc310e658835583b7dbec794d288d
Chloé Lemonnier <lemoloe@gmail.com>
2023-12-19 14:47:48 +0100

    fix img path

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,7 @@ export function About() {
     return (
         
         <div id="about">
-            <img className="pfp" src="../src/assets/images/colibri2.jpg" alt="image de profil avec un colibri"></img>
+            <img className="pfp" src="/assets/images/colibri2.jpg" alt="image de profil avec un colibri"></img>
             <div className="about-desc">
                 <p>
                     Salut, je m'appelle Chloé, aka <span className="red">Neika</span>. Suite à une reconversion avec OpenClassrooms, me voila <span className="red">développeuse web !</span> Je suis <span className="red">curieuse</span> et j'aime <span className="red">apprendre</span>. J'ai une préférence pour <span className="red">React</span> et sa logique par composant.
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,31 +7,31 @@ export function Skills() {
             <h2 className="skills-title">Mes compétences</h2>
             <div className="skills-icons">
                 <Skill
-                    img="../src/assets/icons/html.png"
+                    img="/assets/icons/html.png"
                     name="HTML"
                 />
                 <Skill
-                    img="../src/assets/icons/css.png"
+                    img="/assets/icons/css.png"
                     name="CSS"
                 />
                 <Skill
-                    img="../src/assets/icons/js.png"
+                    img="/assets/icons/js.png"
                     name="Javascript"
                 />
                 <Skill
-                    img="../src/assets/icons/sass.png"
+                    img="/assets/icons/sass.png"
                     name="Sass"
                 />
                 <Skill
-                    img="../src/assets/icons/react.png"
+                    img="/assets/icons/react.png"
                     name="React"
                 />
                 <Skill
-                    img="../src/assets/icons/redux.png"
+                    img="/assets/icons/redux.png"
                     name="Redux"
                 />
                 <Skill
-                    img="../src/assets/icons/swagger.png"
+                    img="/assets/icons/swagger.png"
                     name="Swagger"
                 />
             </div>
